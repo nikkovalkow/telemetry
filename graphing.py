4c866ce9bdac263e6962d2ca49b87ffbdf872d5f
nikkovalkow <nikkovalkow@yandex.ru>
2019-12-09 12:18:38 +0000

    graphing.py

--- graphing.py
+++ graphing.py
@@ -1,8 +1,48 @@
+#!/usr/bin/python3
 import numpy as np
 from bokeh.layouts import gridplot
-from bokeh.plotting import figure, show, output_file
+from bokeh.plotting import figure, show, output_file, save
 from pymongo import MongoClient
 import datetime
+import cgitb
+import cgi
+import datetime
+
+cgitb.enable()
+print('Content-Type: text/html;charset=utf-8')
+print('')
+arguments=cgi.FieldStorage()
+start=str(arguments.getfirst('start','Null')).split('.')
+end=str(arguments.getfirst('end','Null')).split('.')
+
+for i in range (len(start)+1,6):
+        start.append('0')
+
+for i in range (len(end)+1,6):
+        end.append('0')
+
+time_range=[]
+time_range.append(start)
+time_range.append(end)
+
+for time_point in time_range:
+        for i in range(0,len(time_point)):
+                try:
+                        time_point[i]=int(time_point[i])
+                        if time_point[i]>10000: time_point[i]=0
+                except:
+                        time_point[i]=0
+
+try:
+        start = datetime.datetime(time_range[0][0],time_range[0][1],time_range[0][2],time_range[0][3],time_range[0][4])
+except:
+        start = datetime.datetime.now()
+        print('Date paramenter error')
+try:
+        end = datetime.datetime(time_range[1][0],time_range[1][1],time_range[1][2],time_range[1][3],time_range[1][4])
+except:
+        end= datetime.datetime.now()
+        print('Date paramenter error')
 
 
 client = MongoClient('localhost',27017)
@@ -10,8 +50,8 @@ client = MongoClient('localhost',27017)
 db = client['sensors']
 col= db['temperature']
 
-from_date = datetime.datetime(2019,9,19,9,10,30)
-to_date = datetime.datetime(2019,9,21,10,40,30)
+from_date = start
+to_date = end
 
 figures=[]
 
@@ -51,11 +91,13 @@ internet.line(timelist, templist, color='#33cc33', legend='Internet_delay')
     
    
 
-output_file("/var/www/html/sensors.html", title="Sensors")
+output_file("/var/www/html/data/sensors.html", title="Sensors")
 
 figures_list=[]
 for f in figures:
     figures_list.append([f])
 figures_list.append([internet])
 
-show(gridplot(figures_list, plot_width=1800, plot_height=400))
+save(gridplot(figures_list, plot_width=1800, plot_height=400))
+
+print('<a href=/data/sensors.html>Data for '+str(start)+' - '+str(end)+' </a>')
