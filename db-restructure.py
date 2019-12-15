b16ce960891fcae1b3843cf152460f104cdd7392
Mikita_Kavalkou <nikkovalkow@yandex.ru>
2019-12-15 15:45:10 +0300

    sensor-process v3

--- db-restructure.py
+++ db-restructure.py
@@ -8,7 +8,7 @@ col_temp = db['temperature']
 col_data = db['data']
 col_net = db['network']
 print ("Check_Internet - mongodb connection error")
-
+'''
 for record in col_temp.find({'temp':None}):
     col_temp.delete_one(record)
 
@@ -21,5 +21,17 @@ for record in col_net.find():
 
     testdata = {'time': record['time'], 'sensor':4, 'value': record['ms-delay']}
     col_data.insert_one(testdata)
+'''
+for record in col_data.find({'sensor':5}):
+    col_data.delete_one(record)
+for record in col_data.find({'sensor': 6}):
+    col_data.delete_one(record)
+for record in col_data.find({'sensor': 7}):
+    col_data.delete_one(record)
+for record in col_data.find({'sensor': 8}):
+    col_data.delete_one(record)
+for record in col_data.find({'sensor': 9}):
+    col_data.delete_one(record)
+
 
 client.close()
