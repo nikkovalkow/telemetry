5460c8435b05f5723c4b29c2564b9388a49ea9e2
nikkovalkow <nikkovalkow@yandex.ru>
2019-12-19 13:34:55 +0000

    every 2 weeks to learnin

--- copy_to_learnin_week.py
+++ copy_to_learnin_week.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import time
 now = datetime.datetime.now()
-begin = now - datetime.timedelta(days=7)
+begin = now - datetime.timedelta(days=14)
 os.system("curl "+'\"http://192.168.1.110/graphing.py?start='+str(begin.year)+'.'+str(begin.month)+'.'+str(begin.day)+'&end='+str(now.year)+'.'+str(now.month)+'.'+str(now.day+1)+'\"')
 time.sleep(30)
 os.system('scp -o PubkeyAuthentication=yes  /var/www/html/data/sensors.html root@learnin.ru:/var/www/html/sensors_week.html')
