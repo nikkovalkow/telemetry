b9941a70dc085e4598d598099d28aacc2c618725
Mikita_Kavalkou <nikkovalkow@yandex.ru>
2020-02-08 13:58:00 +0300

    hw_platform.py

---
+++ hw_platform.py
@@ -0,0 +1,14 @@
+import os
+
+
+def SendSMS(text):
+    print('sending')
+    os.system('sudo iptables -A FORWARD -s 192.168.10.10 -j DROP')
+    print(os.system('gammu sendsms TEXT +375296799727 -text "' + text + '"'))
+    print('')
+
+def GetHDDSpace():
+    data = os.system('df -h | grep vda')
+    data = data.split(' ')
+    print(data)
+
