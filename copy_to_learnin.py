a253a6e8d22d7a839846801c868f4331e9db1932
Mikita_Kavalkou <nikkovalkow@yandex.ru>
2020-02-09 21:19:30 +0300

    copy to learnin + arg

--- copy_to_learnin.py
+++ copy_to_learnin.py
@@ -11,7 +11,7 @@ if sys.argv[1]=="2DAYS":
     now = now + datetime.timedelta(days=1)
     os.system("python3 graphing.py "+str(begin.year)+'.'+str(begin.month)+'.'+str(begin.day)+' '+str(now.year)+'.'+str(now.month)+'.'+str(now.day)+' 5T N sensors_2days.html')
     time.sleep(30)
-    os.system('scp -o PubkeyAuthentication=yes  /var/www/html/data/sensors_2days.html root@learnin.ru:/var/www/html/sensors_2days.html')
+    os.system('scp -o PubkeyAuthentication=yes  sensors_2days.html root@learnin.ru:/var/www/html/sensors_2days.html')
 
 if sys.argv[1]=="ALL":
     now = datetime.datetime.now()
@@ -19,7 +19,7 @@ if sys.argv[1]=="ALL":
     now = now + datetime.timedelta(days=1)
     os.system("python3 graphing.py " + str(begin.year) + '.' + str(begin.month) + '.' + str(begin.day) + ' ' + str(now.year) + '.' + str(now.month) + '.' + str(now.day) + ' W Y sensors_weekly.html')
     time.sleep(30)
-    os.system('scp -o PubkeyAuthentication=yes  /var/www/html/data/sensors_weekly.html root@learnin.ru:/var/www/html/sensors_weekly.html')
+    os.system('scp -o PubkeyAuthentication=yes  sensors_weekly.html root@learnin.ru:/var/www/html/sensors_weekly.html')
     
 if sys.argv[1]=="2MONTHS":
     now = datetime.datetime.now()
@@ -27,4 +27,4 @@ if sys.argv[1]=="2MONTHS":
     now = now + datetime.timedelta(days=1)
     os.system("python3 graphing.py " + str(begin.year) + '.' + str(begin.month) + '.' + str(begin.day) + ' ' + str(now.year) + '.' + str(now.month) + '.' + str(now.day) + ' D Y sensors_daily.html')
     time.sleep(30)
-    os.system('scp -o PubkeyAuthentication=yes  /var/www/html/data/sensors_daily.html root@learnin.ru:/var/www/html/sensors_daily.html')
+    os.system('scp -o PubkeyAuthentication=yes  sensors_daily.html root@learnin.ru:/var/www/html/sensors_daily.html')
