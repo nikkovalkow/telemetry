7855501e43f462a69c329aca5cc38ff08561d5eb
Mikita_Kavalkou <nikkovalkow@yandex.ru>
2020-02-09 21:15:51 +0300

    copy to learnin + arg

--- copy_to_learnin.py
+++ copy_to_learnin.py
@@ -9,7 +9,7 @@ if sys.argv[1]=="2DAYS":
     now = datetime.datetime.now()
     begin = now - datetime.timedelta(days=1)
     now = now + datetime.timedelta(days=1)
-    os.system("python3 graphing.py "+str(begin.year)+'.'+str(begin.month)+'.'+str(begin.day)+' '+str(now.year)+'.'+str(now.month)+'.'+str(now.day)+'5T N sensors_2days.html')
+    os.system("python3 graphing.py "+str(begin.year)+'.'+str(begin.month)+'.'+str(begin.day)+' '+str(now.year)+'.'+str(now.month)+'.'+str(now.day)+' 5T N sensors_2days.html')
     time.sleep(30)
     os.system('scp -o PubkeyAuthentication=yes  /var/www/html/data/sensors_2days.html root@learnin.ru:/var/www/html/sensors_2days.html')
 
@@ -17,7 +17,7 @@ if sys.argv[1]=="ALL":
     now = datetime.datetime.now()
     begin = now - datetime.timedelta(months=10)
     now = now + datetime.timedelta(days=1)
-    os.system("python3 graphing.py " + str(begin.year) + '.' + str(begin.month) + '.' + str(begin.day) + ' ' + str(now.year) + '.' + str(now.month) + '.' + str(now.day) + 'W Y sensors_weekly.html')
+    os.system("python3 graphing.py " + str(begin.year) + '.' + str(begin.month) + '.' + str(begin.day) + ' ' + str(now.year) + '.' + str(now.month) + '.' + str(now.day) + ' W Y sensors_weekly.html')
     time.sleep(30)
     os.system('scp -o PubkeyAuthentication=yes  /var/www/html/data/sensors_weekly.html root@learnin.ru:/var/www/html/sensors_weekly.html')
     
@@ -25,6 +25,6 @@ if sys.argv[1]=="2MONTHS":
     now = datetime.datetime.now()
     begin = now - datetime.timedelta(months=2)
     now = now + datetime.timedelta(days=1)
-    os.system("python3 graphing.py " + str(begin.year) + '.' + str(begin.month) + '.' + str(begin.day) + ' ' + str(now.year) + '.' + str(now.month) + '.' + str(now.day) + 'D Y sensors_daily.html')
+    os.system("python3 graphing.py " + str(begin.year) + '.' + str(begin.month) + '.' + str(begin.day) + ' ' + str(now.year) + '.' + str(now.month) + '.' + str(now.day) + ' D Y sensors_daily.html')
     time.sleep(30)
     os.system('scp -o PubkeyAuthentication=yes  /var/www/html/data/sensors_daily.html root@learnin.ru:/var/www/html/sensors_daily.html')
