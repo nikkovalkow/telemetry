031d61e93f60ba1bd4335a8c14ce84484ca1dc42
Mikita_Kavalkou <nikkovalkow@yandex.ru>
2020-02-13 19:46:25 +0300

    bug_catch.py

--- bug_catch.py
+++ bug_catch.py
@@ -7,12 +7,12 @@ while(1):
 
     data = os.popen('ping 192.168.1.1 -c 1').read()
 
-    if data.find('1 received') !== -1:
+    if data.find('1 received') == -1:
         print("No connection 192.168.1.1 ",datetime.datetime.now())
 
     data = os.popen('ping 8.8.8.8 -c 1').read()
 
-    if data.find('1 received') !== -1:
+    if data.find('1 received') == -1:
         print("No connection 192.168.1.1 ",datetime.datetime.now())
 
 
