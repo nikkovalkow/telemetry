3bcbf17cb70acb79a0e7591b20b914cabba3344f
Mikita_Kavalkou <nikkovalkow@yandex.ru>
2020-02-13 19:53:32 +0300

    bug_catch.py

--- bug_catch.py
+++ bug_catch.py
@@ -4,20 +4,23 @@ import time
 
 
 while(1):
-    print("pinging 192.168")
-    data = os.popen('ping 192.168.1.1 -c 1').read()
-    print("complete")
+    try:
+        print("pinging 192.168")
+        data = os.popen('ping 192.168.1.1 -c 1').read()
+        print("complete")
 
-    if data.find('1 received') == -1:
-        print("No connection 192.168.1.1 ",datetime.datetime.now())
-    print("pinging 8.8.8.8")
+        if data.find('1 received') == -1:
+            print("No connection 192.168.1.1 ",datetime.datetime.now())
+        print("pinging 8.8.8.8")
 
-    data = os.popen('ping 8.8.8.8 -c 1').read()
-    print("complete")
+        data = os.popen('ping 8.8.8.8 -c 1').read()
+        print("complete")
 
-    if data.find('1 received') == -1:
-        print("No connection 8.8.8.8 ",datetime.datetime.now())
+        if data.find('1 received') == -1:
+            print("No connection 8.8.8.8 ",datetime.datetime.now())
 
-    time.sleep(1)
+        time.sleep(1)
+    except:
+        print("Error in bug_catch.py")
 
 
