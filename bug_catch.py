58a79819d84d894ee2890d2cf0dde8e0a1a0a109
Mikita_Kavalkou <nikkovalkow@yandex.ru>
2020-02-13 19:45:41 +0300

    bug_catch.py

--- bug_catch.py
+++ bug_catch.py
@@ -1,16 +1,18 @@
 import os
 import datetime
 import time
-def PingTest(ip):
-    data = os.popen('ping '+ip+' -c 1').read()
-
-    if data.find('1 received') != -1:
-        return True
-    else:
-        return False
 
 
 while(1):
-    print("192.168.0.1" ,PingTest("192.168.0.1"),datetime.datetime.now())
-    print("8.8.8.8", PingTest("8.8.8.8"),datetime.datetime.now())
+
+    data = os.popen('ping 192.168.1.1 -c 1').read()
+
+    if data.find('1 received') !== -1:
+        print("No connection 192.168.1.1 ",datetime.datetime.now())
+
+    data = os.popen('ping 8.8.8.8 -c 1').read()
+
+    if data.find('1 received') !== -1:
+        print("No connection 192.168.1.1 ",datetime.datetime.now())
+
 
